a1c7b3af4dcb38eaa10acaa22f1c9374eec5089f
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-09-25 20:37:59 -0400

    change code in object oriented

--- index.js
+++ index.js
@@ -1,29 +1,136 @@
+// // Allow multiple accounts to be created
+// // Each account can have many transactions
+// // Allow withdrawals and deposits into accounts
+// // Allow us to retrieve the transaction history of an account (all withdrawals and deposits)
+// // Allow us to retrieve the current balance of the account at any time
+// // Don't allow withdrawals that exceed the remaining balance of the account
+
+
 let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transections = [];
+  }
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;// Calculate the balance using the transaction objects.
   }
 
-  commit() {
-    balance -= this.amount;
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+
+  commit() {
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this);
+    return true;
+  }
+};
+
+
+class deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+};
+class withdrawal extends Transection {
+  get value() {
+    return -this.amount;
+  }
+  isAllowed() {
+    // note how it has access to this.account b/c of parent
+    return (this.account.balance - this.amount >= 0);
+  }
+};
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    // deposits always allowed thanks to capitalism.
+    return true;
+  }
+
+};
+
+// class withdrawal {
+
+//   constructor(amount, account) {
+//     this.amount = amount;
+//     this.account = account;
+//   }
+//   commit() {
+//     this.account.balance -= this.amount;
+//   }
+// }
+// class deposit {
+//   constructor(amount, account) {
+//     this.amount = amount;
+//     this.account = account;
+//   }
+//   commit() {
+//     this.account.balance += this.amount;
+//   }
+// }
+//const myAccount = new account("John");
+
+// console.log('Strating Balance:', myAccount.balance);
+
+// t1 = new withdrawal(50.11, myAccount);
+// //t1.commit();
+
+// console.log('Transection 1:', t1);
+// t2 = new withdrawal(9.99, myAccount.balance);
+//t2.commit();
+// console.log('Transaction 2:', t2);
+// console.log("Balance:", myAccount.balance);
+// t3 = new deposit(60.11, myAccount);
+// //t3.commit();
+// console.log('Transection 3:', t3);
+// t4 = new deposit(19.99, myAccount);
+// //console.log("Balance:", myAccount.balance);
+// //t4.commit();
+// console.log('Transaction 4:', t4);
+// console.log("Ending Balance:", myAccount.balance);
 
 
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account();
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Starting Account Balance: ', myAccount.balance);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-console.log('Balance:', balance);
+console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Withdrawal for 9.99 should be allowed...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('Commit result:', t3.commit());
+
+console.log('Ending Account Balance: ', myAccount.balance);
+console.log("Lookings like I'm broke again");
+
+console.log('Account Transaction History: ', myAccount.transactions);
